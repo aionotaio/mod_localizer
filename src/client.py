018786268d8ef2119c95834bb1e69a40d03c56d7
ethereumvibes <143857085+ethereumvibes@users.noreply.github.com>
2024-09-19 19:34:32 +0800

    Sync version

--- src/client.py
+++ src/client.py
@@ -1,10 +1,8 @@
 import os
-import asyncio
 
-import aiofiles
 import ujson
 import fake_useragent
-import aiohttp
+import requests
 
 from src.jad import JarDecompile
 
@@ -13,52 +11,47 @@ class Client:
     def __init__(self) -> None:
         self.user_agent = fake_useragent.UserAgent().random
 
-    async def look_for_modlist(self, path: str) -> list:
+    def look_for_modlist(self, path: str) -> list:
         mod_list = next(os.walk(path))[2]
         del mod_list[0]
         if len(mod_list) == 0:
             return False
         else:
-            await JarDecompile.look_for_jar(path)
+            JarDecompile.look_for_jar(path)
             return mod_list
 
-    async def load_file_for_translate(self, path: str) -> dict:
-        async with aiofiles.open(path, mode='r') as file:
-            content = await file.read()
-            return ujson.loads(content)
+    def load_file_for_translate(self, path: str) -> dict:
+        with open(path) as file:
+            j = ujson.load(file)
+            return j
 
-    async def translate_request(self, api_key: str, from_: str, to_: str, loaded_json: dict) -> dict:
-        url = "https://microsoft-translator-text.p.rapidapi.com/translate"
-        querystring = {
-            "from": from_,
-            "to": to_,
-            "api-version": "3.0",
-            "profanityAction": "NoAction",
-            "textType": "plain"
-        }
-        headers = {
-            "x-rapidapi-key": api_key,
-            "x-rapidapi-host": "microsoft-translator-text.p.rapidapi.com",
-            "Content-Type": "application/json",
-            "user-agent": self.user_agent
-        }
-        
-        async with aiohttp.ClientSession() as session:
-            payload = [{"Text": val} for val in loaded_json.values()]
-            async with session.post(url, json=payload, headers=headers, params=querystring) as response:
-                data = await response.json()
-                
-        for i, (key, _) in enumerate(loaded_json.items()):
-            loaded_json[key] = data[i]['translations'][0]['text']
-        
+    def translate_request(self, api_key: str, from_: str, to_: str, loaded_json: dict) -> dict:
+        for key, val in loaded_json.items():
+            url = "https://microsoft-translator-text.p.rapidapi.com/translate"
+            querystring = {
+                "from": from_,
+                "to": to_,
+                "api-version": "3.0",
+                "profanityAction": "NoAction",
+			    "textType": "plain"
+            }
+            payload = [{"Text": val}]
+            headers = {
+                "x-rapidapi-key": api_key,
+                "x-rapidapi-host": "microsoft-translator-text.p.rapidapi.com",
+                "Content-Type": "application/json",
+			    "user-agent": self.user_agent
+            }
+            response = requests.post(url, json=payload, headers=headers, params=querystring)
+            loaded_json[key] = response.json()[0]['translations'][0]['text']
         return loaded_json
 
-    async def dump_translated_file(self, path: str, dictionary: dict):
-        async with aiofiles.open(path / 'ru_ru.json', 'w', encoding='utf-8') as f:
-            await f.write(ujson.dumps(dictionary, ensure_ascii=False))
+    def dump_translated_file(self, path: str, dictionary: dict):
+        with (path / 'ru_ru.json').open('x', encoding='utf-8') as f:
+            ujson.dump(dictionary, f, ensure_ascii=False)
 
-    async def change_extension(self, path: str):
+    def change_extension(self, path: str):
         try:
-            os.rename(path, path.with_suffix('.jar'))
+            path.rename(path.with_suffix('.jar'))
         except FileExistsError:
             return None
--- src/jad.py
+++ src/jad.py
@@ -1,31 +1,16 @@
 import os
 import glob
 import zipfile
-import asyncio
-
-import aiofiles
 
 
 class JarDecompile:
-    @staticmethod
-    async def unzip_jar(jar):
-        with zipfile.ZipFile(jar) as file:
-            extract_path = os.path.splitext(jar)[0]
-            for file_info in file.infolist():
-                extracted_path = os.path.join(extract_path, file_info.filename)
-                if file_info.is_dir():
-                    os.makedirs(extracted_path, exist_ok=True)
-                else:
-                    os.makedirs(os.path.dirname(extracted_path), exist_ok=True)
-                    async with aiofiles.open(extracted_path, 'wb') as f:
-                        await f.write(file.read(file_info.filename))
+	def unzip_jar(jar):
+		file = zipfile.ZipFile(jar)
+		file.extractall(os.path.splitext(jar)[0])
 
-    @staticmethod
-    async def look_for_jar(path):
-        tasks = []
-        for currentFile in glob.glob(os.path.join(path, "*")):
-            if os.path.isdir(currentFile):
-                tasks.append(JarDecompile.look_for_jar(currentFile))
-            elif os.path.splitext(currentFile)[1] == '.jar':
-                tasks.append(JarDecompile.unzip_jar(currentFile))
-        await asyncio.gather(*tasks)
+	def look_for_jar(path):
+		for currentFile in glob.glob(os.path.join(path, "*")):
+			if os.path.isdir(currentFile):
+				JarDecompile.look_for_jar(currentFile)
+			elif os.path.splitext(currentFile)[1] == '.jar':
+				JarDecompile.unzip_jar(currentFile)
